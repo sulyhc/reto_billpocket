a6756411a993f083acb35048f95fd66e1f81dd1e
walter <walt.sulyhc@gmail.com>
2017-07-08 15:38:20 -0500

    add readme file and change settings file

--- views/js/functions.js
+++ views/js/functions.js
@@ -1,6 +1,10 @@
 /**
  * Created by walter on 8/07/17.
  */
+
+/**
+ * load user games table by ajax
+ */
 function displayUserTable(){
         var url = "http://localhost/billpocket/user_details/";
         var steamUser = $('#inputPlayerNick').val();
@@ -8,13 +12,19 @@ function displayUserTable(){
         type:'GET',
         url: url,
         data: {'action':'user_history','id':steamUser},
+            beforeSend:function(){
+                $('#displayResults').html('LOADING....');
+            },
         success:function (data) {
-
             $('#displayResults').html(data);
         }
     });
 }
-
+/**
+ * Show popup with stat information of user game
+ * @param steamID
+ * @param appId
+ */
 function displayModalGame(steamID, appId){
     var url = "http://steamcommunity.com/profiles/" + steamID + "/stats/" + appId;
     window.open(url, 'user stats','height=600,width=800');
